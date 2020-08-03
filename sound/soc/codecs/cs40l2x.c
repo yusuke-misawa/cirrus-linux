CirrusLogic/linux-drivers

ASoC: cs40l2x: Wakeup the device if in hibernation
Wake the device on stream startup if the device is in the
hibernation state.

Change-Id: I5e423a220a5005c1ff635bb3add582917e144aa1
Signed-off-by: Paul Handrigan <Paul.Handrigan@cirrus.com>

sound/soc/codecs/cs40l2x.c
@@ -119,25 +119,70 @@ static int cs40l2x_swap_ext_clk(struct cs40l2x_codec *cs40l2x_codec,
 					CS40L2X_PWRCTL_WAKE);
 }
 
+static int cs40l2x_wake(struct cs40l2x_codec *cs40l2x)
+{
+	struct cs40l2x_private *core = cs40l2x->core;
+	struct regmap *regmap = cs40l2x->regmap;
+	struct device *dev = cs40l2x->dev;
+	int i, ret = 0;
+	unsigned int val, reg;
+
+	if (!core->dsp_reg)
+		return -EINVAL;
+
+	reg = core->dsp_reg(core, "POWERSTATE",
+					CS40L2X_XM_UNPACKED_TYPE,
+					core->fw_desc->id);
+	if (!reg)
+		return -EINVAL;
+
+	for (i = 0; i < CS40L2X_STATUS_RETRIES; i++) {
+		ret = regmap_read(regmap, reg, &val);
+		if (!ret)
+			break;
+
+		usleep_range(5000, 5100);
+	}
+
+	if (ret) {
+		dev_err(dev, "Could not read power state\n");
+		return ret;
+	}
+
+	if (val == CS40L2X_POWERSTATE_HIBERNATE) {
+		if (!core->hiber_cmd)
+			return -EINVAL;
+
+		ret = core->hiber_cmd(core, CS40L2X_POWERCONTROL_WAKEUP);
+	}
+
+	return ret;
+}
+
 static int cs40l2x_clk_en(struct snd_soc_dapm_widget *w,
 			struct snd_kcontrol *kcontrol, int event)
 {
 	struct snd_soc_component *comp = snd_soc_dapm_to_component(w->dapm);
 	struct cs40l2x_codec *codec = snd_soc_component_get_drvdata(comp);
+	struct cs40l2x_private *core = codec->core;
 	struct device *dev = codec->dev;
 	int ret = 0;
 
 	mutex_lock(&codec->core->lock);
 	switch (event) {
 	case SND_SOC_DAPM_POST_PMU:
+		ret = cs40l2x_wake(codec);
+		if (ret)
+			goto err;
+
 		ret = cs40l2x_swap_ext_clk(codec, CS40L2X_SCLK);
 		if (ret)
 			goto err;
 
-		codec->core->a2h_enable = true;
+		core->a2h_enable = true;
 		break;
 	case SND_SOC_DAPM_PRE_PMD:
-		codec->core->a2h_enable = false;
+		core->a2h_enable = false;
 		break;
 	default:
 		dev_err(dev, "Invalid event %d\n", event);

yusuke-misawa/cirrus-linux

sound/soc/codecs/cs40l2x.c
@@ -125,32 +125,77 @@ static int cs40l2x_swap_ext_clk(struct cs40l2x_codec *cs40l2x_codec,
 					CS40L2X_PWRCTL_WAKE);
 }
 
+static int cs40l2x_wake(struct cs40l2x_codec *cs40l2x)
+{
+	struct cs40l2x_private *core = cs40l2x->core;
+	struct regmap *regmap = cs40l2x->regmap;
+	struct device *dev = cs40l2x->dev;
+	int i, ret = 0;
+	unsigned int val, reg;
+
+	if (!core->dsp_reg)
+		return -EINVAL;
+
+	reg = core->dsp_reg(core, "POWERSTATE",
+					CS40L2X_XM_UNPACKED_TYPE,
+					core->fw_desc->id);
+	if (!reg)
+		return -EINVAL;
+
+	for (i = 0; i < CS40L2X_STATUS_RETRIES; i++) {
+		ret = regmap_read(regmap, reg, &val);
+		if (!ret)
+			break;
+
+		usleep_range(5000, 5100);
+	}
+
+	if (ret) {
+		dev_err(dev, "Could not read power state\n");
+		return ret;
+	}
+
+	if (val == CS40L2X_POWERSTATE_HIBERNATE) {
+		if (!core->hiber_cmd)
+			return -EINVAL;
+
+		ret = core->hiber_cmd(core, CS40L2X_POWERCONTROL_WAKEUP);
+	}
+
+	return ret;
+}
+
 static int cs40l2x_clk_en(struct snd_soc_dapm_widget *w,
 			struct snd_kcontrol *kcontrol, int event)
 {
 	struct snd_soc_codec *codec = snd_soc_dapm_to_codec(w->dapm);
 	struct cs40l2x_codec *cs40l2x_codec = snd_soc_codec_get_drvdata(codec);
+	struct cs40l2x_private *core = cs40l2x_codec->core;
 	struct device *dev = cs40l2x_codec->dev;
 	int ret = 0;
 
-	mutex_lock(&cs40l2x_codec->core->lock);
+	mutex_lock(&core->lock);
 	switch (event) {
 	case SND_SOC_DAPM_POST_PMU:
+		ret = cs40l2x_wake(cs40l2x_codec);
+		if (ret)
+			goto err;
+
 		ret = cs40l2x_swap_ext_clk(cs40l2x_codec, CS40L2X_SCLK);
 		if (ret)
 			goto err;
 
-		cs40l2x_codec->core->a2h_enable = true;
+		core->a2h_enable = true;
 		break;
 	case SND_SOC_DAPM_PRE_PMD:
-		cs40l2x_codec->core->a2h_enable = false;
+		core->a2h_enable = false;
 		break;
 	default:
 		dev_err(dev, "Invalid event %d\n", event);
 		ret = -EINVAL;
 	}
 err:
-	mutex_unlock(&cs40l2x_codec->core->lock);
+	mutex_unlock(&core->lock);
 	return ret;
 }
 